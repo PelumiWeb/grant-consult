2f7646745856044920e2186bd49405e8e2bed1ae
PelumiDev <Pelumiogundipe905@gmail.com>
2025-02-07 02:26:08 +0100

    grants responsiveness

--- src/app/[locale]/payments/page.tsx
+++ src/app/[locale]/payments/page.tsx
@@ -12,208 +12,196 @@ export default function Home() {
   return (
     <div className="w-full">
       {/*  */}
-      <div className="flex  lg:flex-row justify-center lg:justify-between items-start w-full  flex-wrap px-4">
+      {/* <div className="flex  lg:flex-row justify-center lg:justify-between items-start w-full  flex-wrap px-4">
         {/* left */}
-        <div className="w-full lg:w-[45%] mt-16 ">
-          <div className="bg-white p-8 rounded-[10px]">
-            <h3 className="text-buttonPrimary text-">Complete Your Payment</h3>
-            <p className="text-secondaryTextColor">
-              Finalize your subscription and unlock full access to all grant
-              opportunities and consultant services
-            </p>
-          </div>
 
-          <div className="bg-white px-8 mt-4 h-full lg:h-[600px] py-10 rounded-[10px]">
-            <h3 className="">Order Summary</h3>
-            <p className="text-secondaryTextColor font-bold text-[16px] leading-[19px] mt-4">
-              Customer's Details
-            </p>
+      {/* Right */}
 
-            <div className="flex items-center my-2 ">
-              <p className="text-secondaryTextColor font-bold text-[16px] mr-2">
-                Name:
+      {/* </div> */}
+      <div className="py-8">
+        <h1 className="text-secondaryColor font-semibold text-[20px] lg:text-[36px] leading-[20px] lg:leading-[42px] text-center ">
+          Complete Your Payment
+        </h1>
+        <p className="text-center text-textColor font-normal text-[14px] md:text-[16px] leading-[23px]">
+          Finalize your subscription and unlock full access to all grant
+          opportunities and consultant services
+        </p>
+      </div>
+
+      <div className="bg-white p-8">
+        {/* Left  */}
+        <div>
+          <h3 className="text-primary font-semibold text-[24px] leading-[36px] my-4">
+            {" "}
+            Secure Your Payment with Your Preferred Method
+          </h3>
+          <p className="text-textColor text-[16px] leading-[20px] my-4">
+            Select a payment method, and we’ll guide you through a secure
+            process{" "}
+          </p>
+          <div className="flex items-start justify-between w-full flex-col md:flex-row">
+            {/* Left */}
+            <div className="bg-backgroundColor w-full md:w-[45%] h-[656px] p-4">
+              <h4 className="font-semibold text-[20px] leading-[26px] text-primary mb-4">
+                Order Summary
+              </h4>
+
+              <p className="font-semibold text-[16px] leading-[19px] text-primaryBlack ">
+                Customer’s Details
               </p>
-              <p className="text-secondaryTextColor text-[16px]">
-                Adams Benson
-              </p>
+
+              <div className="border-b-[0.5px] border-b-borderColor py-2 my-2">
+                <div className="flex items-center justify-between">
+                  <p className="font-medium text-[14px] leading-[25px] text-textColor ">
+                    Name
+                  </p>
+                  <p className="font-medium text-[14px] leading-[25px] text-textColor">
+                    Williams Smith
+                  </p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <p className="font-medium text-[14px] leading-[25px] text-textColor ">
+                    Phone
+                  </p>
+                  <p className="font-medium text-[14px] leading-[25px] text-textColor">
+                    +234899033456
+                  </p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <p className="font-medium text-[14px] leading-[25px] text-textColor ">
+                    Email:
+                  </p>
+                  <p className="font-medium text-[14px] leading-[25px] text-textColor">
+                    smith@gmail.com
+                  </p>
+                </div>
+              </div>
+
+              {/* Subscription Details */}
+              <div className="mt-4 border-b-[0.5px] border-b-borderColor py-2 my-2">
+                <h4 className="font-semibold text-[20px] leading-[26px] text-primary mb-4">
+                  Subscription Details
+                </h4>
+
+                <p className=" text-[16px] leading-[20px] text-textColor mb-4 font-medium">
+                  Selected Plan: Monthly
+                </p>
+
+                <h4 className="font-semibold text-[16px] leading-[18px] text-primary mb-4">
+                  Plan Benefits
+                </h4>
+                {/* Plan Benefits */}
+                <div>
+                  <div className="flex items-center my-2">
+                    <img
+                      src="/check.svg"
+                      alt=""
+                      className="w-[25px] h-[12px]"
+                    />
+                    <p className="ml-2 font-medium text-[14px] leading-[16px] text-textColor">
+                      Full access to grant listings and personalized dashboard
+                    </p>
+                  </div>
+                  <div className="flex items-center my-2">
+                    <img
+                      src="/check.svg"
+                      alt=""
+                      className="w-[25px] h-[12px]"
+                    />
+                    <p className="ml-2 font-medium text-[14px] leading-[16px] text-textColor">
+                      Tailored grant opportunities matched to your profile
+                    </p>
+                  </div>
+                  <div className="flex items-center my-2">
+                    <img
+                      src="/check.svg"
+                      alt=""
+                      className="w-[25px] h-[12px]"
+                    />
+                    <p className="ml-2 font-medium text-[14px] leading-[16px] text-textColor">
+                      1 free monthly consultation
+                    </p>
+                  </div>
+                  <div className="flex items-center my-2">
+                    <img
+                      src="/check.svg"
+                      alt=""
+                      className="w-[25px] h-[12px]"
+                    />
+                    <p className="ml-2 font-medium text-[14px] leading-[16px] text-textColor">
+                      Weekly newsletter updates
+                    </p>
+                  </div>
+                </div>
+                <div></div>
+              </div>
+              {/* Total */}
+              <div className="flex items-center justify-between w-full mt-2">
+                <h4 className="font-semibold text-[20px] leading-[20px] text-primary mb-4">
+                  Total
+                </h4>
+                <h4 className="font-semibold text-[20px] leading-[20px] text-primary mb-4">
+                  $25.00
+                </h4>
+              </div>
+              {/*  */}
+              <button className="border-[2px] border-secondaryColor flex items-center justify-center h-[55px] w-full rounded-[5px]">
+                <div className="flex items-center ">
+                  <div className="relative w-[16px] h-[16px]">
+                    <Image src={"/black-secondary.svg"} alt="" fill />
+                  </div>
+                  <p className="font-semibold text-[16px] leading-[18px] font-mono text-secondaryColor ml-2">
+                    Change Plan
+                  </p>
+                </div>
+              </button>
             </div>
-            <div className="flex items-center my-2 ">
-              <p className="text-secondaryTextColor font-bold text-[16px] mr-2">
-                Phone:
-              </p>
-              <p className="text-secondaryTextColor text-[16px]">
-                +2348199012348
-              </p>
-            </div>
-            <div className="flex items-center my-2 ">
-              <p className="text-secondaryTextColor font-bold text-[16px] mr-2">
-                Email:
-              </p>
-              <p className="text-secondaryTextColor text-[16px]">
-                Jondoe@gmail.com
-              </p>
-            </div>
-            <p className="text-secondaryTextColor font-bold text-[16px] leading-[19px] my-2 ">
-              Subscription Plan Details
-            </p>
-            <div className="flex items-center my-2 ">
-              <p className="text-secondaryTextColor font-bold text-[16px] mr-2">
-                Plan Selected:
-              </p>
-              <p className="text-secondaryColor text-[16px]">Basic</p>
-            </div>
-            <div className=" my-2 ">
-              <p className=" text-secondaryTextColor font-bold text-[16px] mr-2">
-                Plan Benefits:
-              </p>
-              <p className=" w-[50%] text-secondaryTextColor text-[14px] leading-[23px]">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
-                illo tempora unde ex nulla fuga odit cumque commodi rem, tempore
-                iure iusto alias sapiente veniam animi sed delectus ut
-                obcaecati.
-              </p>
-            </div>
-            <div className="flex items-center my-2 ">
-              <p className="text-secondaryTextColor font-bold text-[16px] mr-2">
-                Plan Price:
-              </p>
-              <p className="text-red-500 text-[16px]">$50</p>
-            </div>
-            <div className="flex items-center my-2  ">
-              <p className="text-secondaryTextColor font-bold text-[16px] mr-2">
-                Discount:
-              </p>
-              <p className="text-red-500 text-[16px]"></p>
-            </div>
-            <div className="flex items-center ">
-              <p className="text-secondaryTextColor font-bold text-[16px] mr-2">
-                Total:
-              </p>
-              <p className="text-red-500 text-[16px]">$50</p>
-            </div>
-            <div className="flex items-center mt-12 cursor-pointer">
-              <ArrowLeftOutlined style={{ fontSize: "16px", color: "#08c" }} />
-              <p className="text-secondaryColor ml-4 ">Change Plan</p>
+            {/* Right */}
+            <div className="w-full md:w-[45%] mt-4 md:mt-0">
+              <h4 className="font-semibold text-[20px] leading-[26px] text-primary mb-4">
+                Payment Method
+              </h4>
+              <div>
+                <div className="w-full h-[60px] rounded-[5px] border-borderColor border-[0.5px] flex items-center px-4 my-4">
+                  <div className="w-[20px] h-[20px] rounded-full border-secondaryColor border-[6px] bg-white" />
+                  <p className="font-medium text-[16px] leading-[19px] ml-4">
+                    Credit/Debit Card
+                  </p>
+                </div>
+                <div className="w-full h-[60px] rounded-[5px] border-borderColor border-[0.5px] flex items-center px-4 my-4">
+                  <div className="w-[20px] h-[20px] rounded-full border-secondaryColor border-[6px] bg-white" />
+                  <img src="/paypalIcon.svg" alt="" className="ml-4" />
+                </div>
+
+                <CustomButton
+                  backgrounColor="bg-secondaryColor"
+                  width="w-full"
+                  textStyle="font-semibold text-[16px] leading-[18px] font-mono text-white"
+                  height="h-[54px]"
+                  title="Proceed to Payment"
+                  IconLeft="/lock.svg"
+                />
+                <div className="flex items-center justify-center">
+                  <p className="text-[14px] leading-[22px] text-center font-mono mt-4 w-[70%]">
+                    You will be redirected to complete your payment securely. No
+                    card details are stored on our platform.
+                  </p>
+                </div>
+
+                <div className="w-full flex items-center justify-center mt-4">
+                  <img src="/Logotype.svg" alt="" />
+                  <img src="/Logotype2.svg" alt="" />
+                  <img src="/Logotype3.svg" alt="" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
         {/* Right */}
-        <div className="w-full lg:w-[45%]">
-          <div className="w-full bg-white px-4 lg:px-16 mt-16">
-            <h3 className="text-primary py-4">Payment Method</h3>
-            <CustomButton
-              width="w-full lg:w-[432px]"
-              height="h-[50px]"
-              title="PayPal"
-              backgrounColor="bg-secondaryColor"
-            />
-            <div className="flex items-center justify-between w-full lg:w-[430px] my-4">
-              <div className="w-full lg:w-[111px] h-[1px] bg-borderColor" />
-              <p className="font-poppins text-secondaryTextColor text-center w-[250px] text-sm ">
-                Or Pay Another Way
-              </p>
-              <div className="w-full lg:w-[111px] h-[1px] bg-borderColor" />
-            </div>
-            <div>
-              <div className="border bg-white border-secondaryColor rounded-[10px] w-full lg:w-[431px] h-[51px] flex items-center justify-center my-4">
-                <img src="/credit-card.svg" />
-                <p className="text-secondaryColor text-center leading-[23px] ml-1">
-                  Card
-                </p>
-              </div>
-            </div>
-            {/* Card Information */}
-            <div>
-              <p className="text-textColor py-1">Card information</p>
-              <div className="w-full lg:w-[430px]  rounded-[10px] border border-borderColor ">
-                {/* Top */}
-                <div className="flex items-center border-b border-b-borderColor justify-between py-1 ">
-                  <Input
-                    type="card"
-                    className="border-none focus:outline-none w-[90%] 
-                  focus:border-none
-                  active:border-none
-                  "
-                    placeholder="0000 0000 0000 0000"
-                  />
-                  <div className="flex items-center justify-around w-[150px]">
-                    <img src="/card-visa.png" />
-                    <img src="/card-master.svg" />
-                    <img src="/card-pay.svg" />
-                  </div>
-                </div>
-                {/* bottom */}
-                <div className="flex items-center h-[40px]">
-                  <Input
-                    className="border-none focus:outline-none w-[90%] 
-                  focus:border-none
-                  active:border-none
-                  h-full
-                  "
-                    placeholder="MM/YY"
-                  />
-                  <Input
-                    className="border-r-none focus:outline-none w-[90%] 
-                  focus:border-r-none
-                  active:border-r-none
-                  h-full
-                  border-b-none
-                  border-t-none
 
-                  "
-                    placeholder="CVV"
-                  />
-                </div>
-              </div>
-            </div>
-
-            {/* Card holder name */}
-            <div className="w-full lg:w-[430px] py-2">
-              <p className="text-textColor py-1">Cardholder Name</p>
-              <Input placeholder="Full Name on card" className="py-4" />
-            </div>
-            <div className="w-full lg:w-[430px] py-2">
-              <p className="text-textColor py-1"> Country or Region</p>
-              <Input placeholder="Nigeria" className="py-4" />
-            </div>
-
-            <div className="flex items-center p-4 ">
-              <Checkbox style={{ fontSize: "22px", color: "#08c" }} />
-              <p className="text-secondaryColor pl-4">
-                Securely save my card details for Next time
-              </p>
-            </div>
-            {/* Button */}
-
-            <div>
-              <CustomButton title="Complete Payment" width="w-full lg:w-[431px]" />
-            </div>
-
-            <p className="text-borderColor text-center leading-[23px] py-4 w-full lg:w-[431px]">
-              By confirming your subscription, you allow Grantsconsult to charge
-              you for future payments in accordance with their terms. You can
-              always cancel your subscription.{" "}
-            </p>
-
-            <div className="py-4 flex justify-center items-center w-full lg:w-[431px]">
-              <p className="text-textColor font-poppins font-normal text-[10px] leading-[17px]">
-                Powered by Flutterwave{" "}
-              </p>
-              <div className="w-[2px] h-[33px] bg-borderColor  ml-2" />
-              <p className="font-poppins underline leading-[16px] text-[10px]  ml-2 ">
-                Terms
-              </p>
-              <p className="font-poppins underline leading-[16px] text-[10px] ml-2">
-                Condition
-              </p>
-            </div>
-          </div>
-        </div>
+        <div></div>
       </div>
-
-      {/* <Faq /> */}
-      <GrantFunder />
     </div>
   );
 }
