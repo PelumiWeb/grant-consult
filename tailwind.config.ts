c0a75a95168b46316133f830ca96374fb22906e6
PelumiDev <Pelumiogundipe905@gmail.com>
2025-02-14 01:16:16 +0100

    fixed bgColor black

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    darkMode: 'class',
+    // darkMode: 'class',
     screens: {
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
