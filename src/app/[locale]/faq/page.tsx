4695ec77a61ff4ae1de069b0bd296e04c8004f06
PelumiDev <Pelumiogundipe905@gmail.com>
2025-02-19 00:41:50 +0100

    edit signup

--- src/app/[locale]/faq/page.tsx
+++ src/app/[locale]/faq/page.tsx
@@ -6,12 +6,14 @@ import CustomButton from "@/app/[locale]/components/CustomButton";
 import { Collapse } from "antd";
 import { MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
 import { useAppDispatch, useAppSelector } from "../../../../lib/hooks";
+import { useLocale } from "next-intl";
 const { Panel } = Collapse;
 
 type Props = {};
 
 const page = (props: Props) => {
   const dispatch = useAppDispatch();
+  const locale = useLocale()
 
   return (
     <div
@@ -131,35 +133,94 @@ const page = (props: Props) => {
                 <Panel
                   className=""
                   // header="What is included in each subscription plan?"
-                  header={"What is included in each subscription plan?"}
+                  header={"What type of opportunities exist on GrantConsult?"}
                   key="1">
-                  <div className=" py-8 m-0">
+                  <div className=" m-0">
                     <p className="font-poppins leading-[20px] text-[12px]  ml-8">
-                      Vivamus sit amet interdum elit. Proin lacinia erat ac
-                      velit tempus auctor.
+                      On GrantConsult, you will find a wide range of
+                      opportunities, including grants, scholarships,
+                      fellowships, internships, and funding programmes for
+                      students, researchers, entrepreneurs and organizations.
                     </p>
                   </div>
                 </Panel>
-                <Panel header="How does the 7-day free trial work?" key="2">
+                <Panel
+                  header="Are the opportunities available to people from all countries?"
+                  key="2">
                   <p className="font-poppins leading-[20px] text-[12px] ">
-                    Vivamus sit amet interdum elit. Proin lacinia erat ac velit
-                    tempus auctor.
+                    GrantsConsult is a global platform. There are grants from
+                    all countries and regions of the world. All you have to do
+                    is use the search filter to find opportunities for your
+                    country or region.
+                  </p>
+                </Panel>
+                <Panel header="How often is the website updated?" key="3">
+                  <p className="font-poppins leading-[20px] text-[12px] ">
+                    GrantsConsult platform is updated daily with the most
+                    current grants and funding opportunities.
+                  </p>
+                </Panel>
+
+                <Panel
+                  header=" How can I verify the legitimacy of an opportunity?"
+                  key="5">
+                  <p className="font-poppins leading-[20px] text-[12px] ">
+                    We strive to verify all listings before publishing. However,
+                    we encourage users to double-check details on the official
+                    websites linked to each listing.
                   </p>
                 </Panel>
                 <Panel
-                  header="What is included in each subscription plan?"
-                  key="3">
+                  header="Are there deadlines for these opportunities?"
+                  key="6">
                   <p className="font-poppins leading-[20px] text-[12px] ">
-                    Vivamus sit amet interdum elit. Proin lacinia erat ac velit
-                    tempus auctor.
+                    Mostly. All deadlines are listed in the description of each
+                    opportunity.
                   </p>
                 </Panel>
                 <Panel
-                  header="What is included in each subscription plan?"
-                  key="3">
+                  header="Is it free to access opportunities on this website?"
+                  key="7">
                   <p className="font-poppins leading-[20px] text-[12px] ">
-                    Vivamus sit amet interdum elit. Proin lacinia erat ac velit
-                    tempus auctor.
+                    Yes, some published opportunities are free. However, you
+                    have to be a registered user on GrantsConsult or on a paid
+                    subscription to explore details on premium opportunities.
+                  </p>
+                </Panel>
+
+                <Panel
+                  header="Do you charge for applying to scholarships or grants?"
+                  key="8">
+                  <p className="font-poppins leading-[20px] text-[12px] ">
+                    GrantsConsult only charges fees for personalized services
+                    such as grant writing, consultation, proposal review and
+                    feedback and other. Please bear in mind that some
+                    applications may come with an application fee which is not
+                    inclusive of GrantsConsult service charge for any service
+                    rendered.
+                  </p>
+                </Panel>
+                <Panel header="I missed a deadline. Can I still apply?" key="9">
+                  <p className="font-poppins leading-[20px] text-[12px] ">
+                    Unfortunately, once a deadline has passed, you cannot apply.
+                    We recommend setting up alerts to avoid missing future
+                    opportunities.
+                  </p>
+                </Panel>
+                <Panel
+                  header="Who can I contact if I have questions about an opportunity? "
+                  key="10">
+                  <p className="font-poppins leading-[20px] text-[12px] ">
+                    Our support team is available to answer your questions.
+                    (info@grantsconsult.com, +2349106288373)
+                  </p>
+                </Panel>
+                <Panel
+                  header="Can I suggest an opportunity to be listed?"
+                  key="11">
+                  <p className="font-poppins leading-[20px] text-[12px] ">
+                    Yes, we welcome suggestions! You can submit opportunities
+                    via our email (info@grantsconsult.com)
                   </p>
                 </Panel>
               </Collapse>
@@ -182,36 +243,75 @@ const page = (props: Props) => {
                 <Panel
                   className=""
                   // header="What is included in each subscription plan?"
-                  header={"What is included in each subscription plan?"}
+                  header={"How can I find opportunities that match my profile?"}
                   key="1">
                   <div className=" py-8 m-0">
                     <p className="font-poppins leading-[20px] text-[12px]  ">
-                      Vivamus sit amet interdum elit. Proin lacinia erat ac
-                      velit tempus auctor.
+                      You can use our filters to search by keyword, funding
+                      type, industry, months, country, field of study, etc
                     </p>
                   </div>
                 </Panel>
-                <Panel header="How does the 7-day free trial work?" key="2">
+                <Panel
+                  header="Do I need to create an account to access opportunities?"
+                  key="2">
                   <p className="font-poppins leading-[20px] text-[12px] ">
-                    Vivamus sit amet interdum elit. Proin lacinia erat ac velit
-                    tempus auctor.
+                    Yes. By creating an account with GrantsConsult you will
+                    unlock all available opportunities and the full features of
+                    the platform, such as personalised dashboard, saved
+                    searches, alerts, newsletters and recommendations
                   </p>
                 </Panel>
                 <Panel
-                  header="What is included in each subscription plan?"
+                  header="Do you assist with the application process?"
                   key="3">
                   <p className="font-poppins leading-[20px] text-[12px] ">
-                    Vivamus sit amet interdum elit. Proin lacinia erat ac velit
-                    tempus auctor.
+                    That's a big YES! At GrantsConsult we have professional
+                    grant consultants who are specialists with the grant
+                    application process and other advisory services. Find our
+                    packages{" "}
+                    <a className="mx-1" href={`${locale}/subscription`}>
+                      here
+                    </a>
+                    . Additional services include Grant Research &
+                    Identification, Proposal Review and Feedback, Training &
+                    Workshops
                   </p>
                 </Panel>
                 <Panel
-                  header="What is included in each subscription plan?"
-                  key="3">
-                  <p className="font-poppins leading-[20px] text-[12px] ">
-                    Vivamus sit amet interdum elit. Proin lacinia erat ac velit
-                    tempus auctor.
-                  </p>
+                  header="I am a Grant Donor, how can I publish my grant or other funding opportunities on GrantsConsult?"
+                  key="4">
+                  <ul className="list-disc text-black">
+                    <p className="font-poppins leading-[20px] text-[12px] ">
+                      Follow our quick and easy steps to give your grants the
+                      deserved visibility.
+                    </p>
+                    <li className="ml-6">
+                      <p className="font-poppins leading-[20px] text-[12px] ">
+                        Register and sign up as a donor grantor
+                      </p>
+                    </li>
+                    <li className="ml-6">
+                      <p className="font-poppins leading-[20px] text-[12px] ">
+                        List your grants.
+                      </p>
+                    </li>
+                    <li className="ml-6">
+                      <p className="font-poppins leading-[20px] text-[12px] ">
+                        Your submission will be reviewed and published if it
+                        meets the GrantsConsult criteria.
+                      </p>
+                    </li>
+                    <p className="font-poppins leading-[20px] text-[12px] ">
+                      Kindly click{" "}
+                      <a
+                        className="mx-1 text-secondaryColor"
+                        href={`${locale}/grants/become`}>
+                        here
+                      </a>{" "}
+                      to begin.
+                    </p>
+                  </ul>
                 </Panel>
               </Collapse>
             </div>
